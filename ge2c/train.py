c4218fdafb0cc1e307a94754c0dc045fc1e08e3a
Milad Heidari <milad.h9999@gmail.com>
2025-02-20 03:37:42 -0800

    Add: TQDM

--- ge2c/train.py
+++ ge2c/train.py
@@ -5,6 +5,7 @@ import numpy as np
 import gymnasium as gym
 from pathlib import Path
 from datetime import datetime
+from tqdm.notebook import tqdm
 from torch.distributions.kl import kl_divergence
 from torch.nn.functional import mse_loss
 from torch.nn.utils import clip_grad_norm_
@@ -124,7 +125,7 @@ def train(
     decoder.train()
     transition_model.train()
 
-    for epoch in range(config.num_epochs):
+    for epoch in tqdm(range(config.num_epochs)):
         # train
         encoder.train()
         decoder.train()
